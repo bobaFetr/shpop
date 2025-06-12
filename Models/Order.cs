da0f3c17c92c28745cf1e2b242bf1f9959639716
Boris Petkov <boris.v.bobi.petkoff@gmail.com>
2025-06-12 16:15:49 +0300

    Added the models

---
+++ Models/Order.cs
@@ -0,0 +1,10 @@
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+
+    public int CustomerId { get; set; }
+    public virtual Customer Customer { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; }
+}
--- obj/Debug/net9.0/shpop.AssemblyInfo.cs
+++ obj/Debug/net9.0/shpop.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shpop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aca177b5c33995bb87fbe880c379a6fe59f0bebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("shpop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shpop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
